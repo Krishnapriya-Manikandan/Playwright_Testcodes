d75ff70bc3a20f9e13e19546eafd01d3056f514c
Krishnapriya <priyanair260698@gmail.com>
2023-08-08 14:45:06 +0800

    added new line duplicate at last

--- home.spec.ts
+++ home.spec.ts
@@ -76,7 +76,7 @@ test.describe('Home', () => {
         //took from let path extension
        const searchicon =  await page.locator('//div[@id="header-action"]/ul[1]/li[1]/a[1]/i[1]');
        await expect(searchicon).toBeVisible();
-       
+       await expect(searchicon).toBeVisible();  // just duplicated this line for learning GIT, pls delete later before running
     })
     
 
